bc29b2619f0c8144e3c668450b31e55c9f4eb4e3
hugo catalan <huugo2k05@gmail.com>
2026-02-17 18:56:11 +0100

    Clase 17/2/26 dp

---
+++ cses/dp/book_shop.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int numBooks;
+    int maxPrice;
+    cin >> numBooks;
+    cin >> maxPrice;
+
+    vector<int> books(price);
+    for( int i{}; i < numBooks; i++) cin >> books[maxPrice];
+
+    vector<int> pages(numBooks);
+    for(int i{};i < numBooks; i++)
+
+}
