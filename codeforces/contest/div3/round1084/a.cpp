2d8f580cf679a6d989fa8b6daae79a48b0fb9e5a
hugo catalan <huugo2k05@gmail.com>
2026-02-27 16:17:24 +0100

    Problema A y B contest 1084

--- codeforces/contest/div3/round1084/a.cpp
+++ codeforces/contest/div3/round1084/a.cpp
@@ -5,7 +5,34 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+
+        int max = 0;
+        int aux;
+        for(int i = 0; i < n; i++){
+            cin >> aux;
+            a[i] = aux;
+            if(aux > max){
+                max = aux;
+            }
+        } 
+
+        int res = 0;
+        for(int i : a){
+            if(i == max){
+                res++;
+            }
+        }
+
+        cout << res << "\n";
+    }
 
     return 0;
 }
--- codeforces/contest/div3/round1084/b.cpp
+++ codeforces/contest/div3/round1084/b.cpp
@@ -1,11 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isOrdered(vector<int> nums){
+    for(int i = 0; i < nums.size() - 1; i++){
+        if(!(nums[i] <= nums[i + 1])){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    int t;
+    cin >> t;
+
+    int n;  
+    for(int i = 0; i < t; i++){
+        cin >> n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+
+        if(isOrdered(a)){
+            cout << n << "\n";
+        }else{
+            cout << "1" << "\n";
+        }
+    }
 
     return 0;
 }
