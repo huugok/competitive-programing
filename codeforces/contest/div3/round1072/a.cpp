d9cc9f12f7897a65920d886b0e06a1eb7d1fa132
hugo catalan <huugo2k05@gmail.com>
2026-03-08 17:45:48 +0100

    Problema A round 1072 resuelto

---
+++ codeforces/contest/div3/round1072/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        if(n == 2) cout << "2\n";
+        else if(n == 3) cout << "3\n";
+        else if(n%2 == 0) cout << "0\n";
+        else cout << "1\n";
+    }
+
+    return 0;
+}
